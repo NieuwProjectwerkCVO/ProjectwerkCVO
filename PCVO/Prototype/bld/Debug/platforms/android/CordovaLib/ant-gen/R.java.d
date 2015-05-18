9152d04921d0bca58e161834ff6483551bfaecbb
pancake999 <z3chsy@gmail.com>
2015-05-18 17:35:40 +0200

    Reset

---
+++ PCVO/Prototype/bld/Debug/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
---
+++ PCVO/Prototype/bld/Debug/platforms/android/ant-gen/R.java.d
@@ -0,0 +1,17 @@
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\ant-gen\io\cordova\Prototype\R.java \
+ : C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable\icon.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-land-hdpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-land-ldpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-land-mdpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-land-xhdpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-port-hdpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-port-ldpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-port-mdpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-port-xhdpi\screen.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\values\strings.xml \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\res\xml\config.xml \
+C:\Users\ARISTOCRAT\Desktop\Programmeren 5\Project\PROJECT_SCRATCHPAD\Prototype\bld\Debug\platforms\android\ant-build\AndroidManifest.xml \
