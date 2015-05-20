291efb7728772b798c212952560f4a17fb29d3a0
ErnstHarnie <ernst_harnie@hotmail.com>
2015-05-20 14:55:17 +0200

    Klassen Ernst

---
+++ Klassen/BLL Ernst/StudieBewijsType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace ProjectwerkCursistenportaal.BLL
+{
+    public class StudieBewijsType
+    {
+
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Omschrijving { get; set; }
+
+        
+    }
+}
